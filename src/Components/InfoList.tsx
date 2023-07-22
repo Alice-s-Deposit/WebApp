2bb8c65df91617ffc7b0ed9a3969e076d811dc92
Maxime <sowlution.pro@gmail.com>
2023-07-23 01:36:27 +0200

    fix public

--- src/Components/InfoList.tsx
+++ src/Components/InfoList.tsx
@@ -6,10 +6,9 @@ export const InfoList = () => {
       <ul>
         <li><b>Step 1</b>: Generate <b>ETH burner address</b><br></br>It's saved en encrypted localy with your password.</li>
         <li>⚠️<b>WARNING: COPY AND SAVE THE PUBLIC & PRIVATE KEY</b></li>
-        <li><b>Step 2</b>: Generate a shielded address, <b>zkBOB address</b></li>
+        <li><b>Step 2</b>: <b>Login & Generate</b> a shielded address, <b>zkBOB address</b></li>
         <li>⚠️<b>WARNING: IMPERATIVELY COPY THE MNEMONIC SEED</b></li>
         <li><b>Step 3</b>: Send funds to burner address (ETH only)</li>
-        <li>⚠️<b>WARNING: do not connect the address to an RPC that is not yours or that is not routed.</b></li>
         <li><b>Step 4</b>: Direct deposit</li>
         <li><b>Step 5: to withdraw your funds go on zkBob platform and import a zkAccount : past the generated mnemonic.</b></li>
       </ul>
