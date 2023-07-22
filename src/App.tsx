ae0c2a796521a52a7ac66e835c2bdb27a20f2762
Maxime <sowlution.pro@gmail.com>
2023-07-22 12:59:33 +0200

    add tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ZkClientProvider } from './Context/ZkClient';
 import { Auth } from './Components/Auth';
 import { ShieldedAddressGenerator } from './Components/ShieldedAddressGenerator';
 import { PasswordInput } from './utils/keyManagement'
+import { InfoList } from './Components/InfoList';
 
 function App() {
   const [buttonDisabled, setButtonDisabled] = useState(false);
@@ -24,6 +25,13 @@ function App() {
   return (
     <div className="App">
       <div className="App-title">Cypher Deposit</div>
+      <InfoList />
+      
+      <div className="enclosure">
+      <h2>Generate a new ETH Vanity address</h2>
+        <PasswordInput />
+      </div>
+      
       <div className="enclosure">
         <h2>zkBob <br></br>Generate a new shielded address</h2>
         <ZkClientProvider>
@@ -31,12 +39,10 @@ function App() {
           <ShieldedAddressGenerator handleButtonClick={handleButtonClick} buttonDisabled={buttonDisabled} />
         </ZkClientProvider>
       </div>
-      <div className="enclosure">
-      <h2>Generate a new ETH Vanity address</h2>
-        <PasswordInput />
-      </div>
+
+      <footer></footer>
     </div>
-  );
+     );
 }
 
 export default App;
---
+++ src/Components/InfoList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const InfoList = () => {
+  return (
+    <div className="info-list">
+      <ul>
+        <li><b>Step 1</b>: Generate <b>ETH vanity address</b><br></br>It's saved en encrypted localy with your password.</li>
+        <li>⚠️<b>WARNING: COPY AND SAVE THE PUBLIC & PRIVATE KEY</b></li>
+        <li><b>Step 2</b>: Generate a shielded address, <b>zkBOB address</b></li>
+        <li>⚠️<b>WARNING: IMPERATIVELY COPY THE MNEMONIC SEED</b></li>
+        <li><b>Step 3</b>: Send funds to vanity address (ETH only)</li>
+        <li>⚠️<b>WARNING: do not connect the address to an RPC that is not yours or that is not routed.</b></li>
+        <li><b>Step 4</b>: Direct deposit</li>
+        <li>⚠️<b>WARNING: TO WITHDRAW FROM ZKBOB GO TO ZKBOB AND COPY PASTE YOUR MNEMONIC</b></li>
+      </ul>
+    </div>
+  );
+};
+
